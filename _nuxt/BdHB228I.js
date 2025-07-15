553eb566efa529dff12fe440a12200f5a27ff471
longwebstudio <info@longwebstudio.net>
2025-07-15 03:04:58 +0000

    Deploying to gh-pages from @ longwebstudio/ThietKe@2267139ea2fd08c0e9f84abfde5b415a55b18cd2 🚀

---
+++ _nuxt/BdHB228I.js
@@ -0,0 +1,1 @@
+import{r as t}from"./DHkg8uIm.js";import a from"./Dj7XWIRZ.js";import{y as e,E as n,i as s,w as i,I as c}from"./BZOcpPos.js";import"./BcoM21Dg.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(s(),n(a,{color:"error",icon:c(o).ui.icons.caution},{default:i(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
